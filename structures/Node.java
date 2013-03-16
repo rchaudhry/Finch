19cebf482e414cd888855063b8802b81a7463d85
fireredlink5000 <anthonywong555@gmail.com>
2013-03-16 14:27:49 -0400

    Structures of the Finch

---
+++ structures/Node.java
@@ -0,0 +1,40 @@
+package structures;
+/**
+ * Node Class
+ * @author Anthony Wong 
+ * @date March 12, 2013 
+ */
+public class Node {
+	
+	/**
+	 * The Parent of the Node.
+	 */
+	Node parent;
+	
+	/**
+	 * The "left" of the Node.
+	 */
+	Node left;
+	
+	/**
+	 * The "right" of the Node.
+	 */
+	Node right;
+	
+	/**
+	 * The "straight" of the Node.
+	 */
+	Node straight;
+	
+	/**
+	 * The next of the Node. 
+	 */
+	Node next;
+	
+	/**
+	 * The Queue of the Node. 
+	 */
+	Queue Queue;
+	
+	
+}
---
+++ structures/Queue.java
@@ -0,0 +1,75 @@
+package structures;
+/**
+ * @title Queue
+ * @author Anthony Wong
+ * @date March 12 2013
+ */
+public class Queue {
+	
+	/**
+	 * Front of the Queue
+	 */
+	Node front;
+	/**
+	 * Rear of the Queue
+	 */
+	Node rear;
+	/**
+	 * Size of the Queue
+	 */
+	int size;
+	
+	/**
+	 * Adds the Node to the end of the Queue
+	 * @param Node
+	 */
+	public void enqueue(Node target){
+		if(size == 0){
+			front = target;
+			rear = target;
+		}else{
+			rear.next = target;
+			rear = rear.next;		
+		}
+		size++;
+	}
+	
+	/**
+	 * Deletes the Node at the front of the Queue
+	 * @return Node
+	 */
+	public Node dequeue(){
+		if(size != 0){
+			Node temp = front;
+			front = front.next;
+			size--;
+			return temp;	
+		}else{
+			return null;
+		}
+	}
+	
+	/**
+	 * 
+	 * @return Front of the Queue
+	 */
+	public Node peek(){
+		return front;
+	}
+	
+	/**
+	 * 
+	 * @return Last 
+	 */
+	public Node getRear(){
+		return rear;
+	}
+	
+	/**
+	 * 
+	 * @return The size of the Queue
+	 */
+	public int getSize(){
+		return size;
+	}
+}
